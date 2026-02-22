a33735928b5eed1fb5da7b2dc402897acc6cad3a
Jefersonpatricio <jelsantos37@gmail.com>
2026-02-21 23:33:31 -0300

    feat: enhance layout structure with responsive design and styling

--- app/(routes)/auth/layout.tsx
+++ app/(routes)/auth/layout.tsx
@@ -1,3 +1,9 @@
 export default function layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-amber-50/90 font-sans">
+      <main className="flex flex-col min-h-screen w-full max-w-6xl items-start justify-start py-8 px-8 bg-white rounded-lg">
+        {children}
+      </main>
+    </div>
+  );
 }
